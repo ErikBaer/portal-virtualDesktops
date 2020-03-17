748579eb19a1fe60c2fee8d35e9b15bcb46afa61
Erik Baer <erik.baer@yahoo.de>
2020-03-18 00:03:48 +0100

    update config.js and wsportal.js with specific ressources

--- .history/website/js/config_20200317235814.js
+++ .history/website/js/config_20200317235814.js
@@ -1,13 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-central-1_bbSNWzxmy', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-1_9sk2JEOFq', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '3r2uqq2nq84lp89dkufpkoia1f', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-central-1' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://da9u5x1qhi.execute-api.eu-central-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
-        email: 'e_baer@hotmail.com'
+        email: 'rajesh.ramaswamy@netenrich.com'
     }
 };
