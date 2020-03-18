59136416c5c0106bc8da562636323536798f46ee
Erik Baer <erik.baer@yahoo.de>
2020-03-18 19:00:09 +0100

    update config

--- .history/website/js/config_20200318185757.js
+++ .history/website/js/config_20200318185757.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-central-1_MxOmriNU5', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'eu-central-1_WmisgItk6', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '1l807mnjrpmrepu7h03b3gp2go', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-central-1' // e.g. us-east-2
     },
