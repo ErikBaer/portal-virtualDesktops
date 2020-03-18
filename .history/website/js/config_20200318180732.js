7f1f656b4e340d243102f0b384f8eb2acf992408
Erik Baer <erik.baer@yahoo.de>
2020-03-18 18:33:55 +0100

    edit deploy.json

--- .history/website/js/config_20200318180732.js
+++ .history/website/js/config_20200318180732.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-central-1_WmisgItk6', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'eu-central-1_wPVBIzIiO', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '2lgq1d0af6asa4e3qlq43tl775', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-central-1' // e.g. us-east-2
     },
